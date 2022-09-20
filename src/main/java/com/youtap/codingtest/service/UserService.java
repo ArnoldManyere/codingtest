9a467b69f9e4f3d16e3e51d228a6d63dab32133d
Arnold Manyere <arnoldmanyere@gmail.com>
2022-09-20 11:46:36 +0200

    Created userService and its implementation

---
+++ src/main/java/com/youtap/codingtest/service/UserService.java
@@ -0,0 +1,15 @@
+package com.youtap.codingtest.service;
+
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: arnoldm
+ * Email: arnoldmanyere@gmail.com
+ * Phone: +263 779 056 314
+ * Date: 2022-09-20
+ * Time: 11:45
+ */
+public interface UserService {
+
+}
---
+++ src/main/java/com/youtap/codingtest/service/impl/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package com.youtap.codingtest.service.impl;
+
+import com.youtap.codingtest.service.UserService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: arnoldm
+ * Email: arnoldmanyere@gmail.com
+ * Phone: +263 779 056 314
+ * Date: 2022-09-20
+ * Time: 11:45
+ */
+
+@Service
+public class UserServiceImpl implements UserService {
+}
